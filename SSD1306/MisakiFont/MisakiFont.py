415e955bb4f46177ce984605f62bccbf530cd64f
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2018-09-30 23:39:01 +0900

    update

--- SSD1306/MisakiFont/MisakiFont.py
+++ SSD1306/MisakiFont/MisakiFont.py
@@ -1,68 +1,71 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import Adafruit_GPIO.SPI as SPI
 import Adafruit_SSD1306
-
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
+from PIL import Image, ImageDraw, ImageFont
 import textwrap
 import mojimoji
+
+import time
 
 # $ wget http://www.geocities.jp/littlimi/arc/misaki/misaki_ttf_2015-04-10.zip
 FONT_PATH = '/home/pi/font/misakifont/misaki_gothic.ttf'
 
 class MisakiFont:
-    def __init__(self):
-        self.str = ['','','','','','','','']
-        self.char_width = 8
-        self.char_height = 8
+    def __init__(self, fontsize=8, rst=24):
+        self.fontsize = fontsize
+        self.rst = rst
+
+        self.str = []
+        self.char_width = self.fontsize
+        self.char_height = self.fontsize
         self.cur_row = 0
         self.enable = True
 
-        # Raspberry Pi pin configuration
-        self.rst = 24
-
-        #
+        # initialize display
         self.disp = Adafruit_SSD1306.SSD1306_128_64(rst=self.rst)
         #self.disp = Adafruit_SSD1306.SSD1306_96_16(rst=self.rst)
-
-        # Initialize library.
         try:
             self.disp.begin()
         except:
             self.enable =False
             return
-            
-        # Clear display.
         self.disp.clear()
         self.disp.display()
 
+        # load font
+        self.font = ImageFont.truetype(FONT_PATH, self.fontsize, encoding='unic')
+        (self.char_width, self.char_height) = self.font.getsize('８')
+        self.char_height += 1
+        #print((self.char_width, self.char_height))
+        
+        # cols and rows
+        self.cols = int(self.disp.width / self.char_width)
+        self.rows = int(self.disp.height / self.char_height)
+        for i in range(self.rows):
+            self.str.append("")
+
         # Create blank image for drawing.
         # Make sure to create image with mode '1' for 1-bit color.
-        self.width = self.disp.width
-        self.height = self.disp.height
-        self.image = Image.new('1', (self.width, self.height))
-
-        self.cols = int(self.width / self.char_width)
-        self.rows = int(self.height / self.char_height)
+        self.image = Image.new('1', (self.disp.width, self.disp.height))
 
         # Get drawing object to draw on image.
         self.draw = ImageDraw.Draw(self.image)
  
         # Draw a black filled box to clear the image.
-        self.draw.rectangle((0,0,self.width,self.height), outline=0, fill=0)
+        self.draw.rectangle((0,0,self.disp.width,self.disp.height), outline=0, fill=0)
 
-        self.font = ImageFont.truetype(FONT_PATH, 8, encoding='unic')
 
     def clear(self):
         if not self.enable:
             return
-        self.draw.rectangle((0,0,self.width,self.height), outline=0, fill=0)
+        self.draw.rectangle((0,0,self.disp.width,self.disp.height), outline=0, fill=0)
         self.disp.image(self.image)
         #self.disp.clear()
         self.disp.display()
+        self.cur_row = 0
+        for i in range(self.rows):
+            self.str[i] = ''
 
     def _draw1line(self, col, row, str):
         if not self.enable:
@@ -75,7 +78,7 @@ class MisakiFont:
         if not self.enable:
             return
         self.str[self.cur_row] = str
-        self.draw.rectangle((0,0,self.width,self.height), outline=0, fill=0)
+        self.draw.rectangle((0,0,self.disp.width,self.disp.height), outline=0, fill=0)
         for r in range(self.rows):
             self._draw1line(0, r, self.str[r])
         self.disp.image(self.image)
@@ -95,18 +98,23 @@ class MisakiFont:
 
 if __name__ == '__main__':
     misakifont = MisakiFont()
+    print('font ' + str(misakifont.fontsize))
+    print('char ' + str(misakifont.char_width) + 'x' + str(misakifont.char_height))
+    print('disp ' + str(misakifont.cols) + 'x' + str(misakifont.rows))
     while True:
-        misakifont.println('0123456789')
         misakifont.println('123456789')
         misakifont.println('2あいうえおかきくけこさしすせそたちつてと')
         misakifont.println('3あいうえおABC')
+        time.sleep(3)
         misakifont.clear()
-        misakifont.println('4ガギグゲゴ')
-        misakifont.println('5ｶﾞｷﾞｸﾞｹﾞｺﾞ')
-        misakifont.println('6あいうえお')
-        misakifont.println('7あいうえお')
-        print(misakifont.cur_row)
-        misakifont.println('8あいうえお')
-        print(misakifont.cur_row)
-        misakifont.println('9あいうえお')
-        print(misakifont.cur_row)
+        misakifont.println('0 123456789')
+        misakifont.println('1 ガギグゲゴ')
+        misakifont.println('2 ｶﾞｷﾞｸﾞｹﾞｺﾞ')
+        misakifont.println('3 あいうえお')
+        misakifont.println('4 あいうえお')
+        misakifont.println('5 あいうえお')
+        misakifont.println('6 あいうえお')
+        misakifont.println('7 ' + str(misakifont.fontsize))
+        misakifont.println('8 ' + str(misakifont.char_width) + 'x' + str(misakifont.char_height))
+        misakifont.println('9 ' + str(misakifont.cols) + 'x' + str(misakifont.rows))
+        break
