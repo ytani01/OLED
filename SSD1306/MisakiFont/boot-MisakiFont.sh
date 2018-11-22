04f5af3eb2a8255ddbd1d49e6f572ddbc552e0f8
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2018-11-23 02:47:43 +0900

    update

--- SSD1306/MisakiFont/boot-MisakiFont.sh
+++ SSD1306/MisakiFont/boot-MisakiFont.sh
@@ -5,8 +5,13 @@ MYNAME=`basename $0`
 
 BINDIR=${HOME}/bin
 LOGDIR=${HOME}/tmp
+ENVDIR=${HOME}/env
+ENVBIN=${ENVDIR}/bin
 
 MISAKI_FONT=${BINDIR}/MisakiFont.py
+if [ -x ${ENVBIN}/MisakiFont.py ]; then
+    MISAKI_FONT=${ENVBIN}/MisakiFont.py
+fi
 
 LOGFILE=${LOGDIR}/${MYNAME}.log
 
@@ -14,6 +19,9 @@ if [ ! -d ${LOGDIR} ]; then
     mkdir ${LOGDIR}
 fi
 
+if [ -f ${ENVBIN}/activate ]; then
+    . ${ENVBIN}/activate
+fi
 cd ${BINDIR}
 date > ${LOGFILE} 2>&1
 ${MISAKI_FONT} >> ${LOGFILE} 2>&1
